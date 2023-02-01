58bd01b373d827883a24139a189726b7d464590b
ulus karakoc <uluskk@gmail.com>
2023-02-02 00:29:51 +0100

    aufgabe wird vorbereitet

---
+++ JS Loop For/selbst übungen/solution.js
@@ -0,0 +1,27 @@
+let sayilar = [1, 5, 7, 15, 3, 25, 12, 24];
+
+// 1- Schreiben Sie der Quadratwurzel jedes Element in der Zahlen?
+
+// 2- Welche Zahlen ist 5 mal große von 5 auf dem Zahlenlist
+
+// 3- Finden Sie die summe der gerade Zahlen auf dem Zahlen list
+
+let produkte = [
+  "iphone 12",
+  "samsung s22",
+  "iphone 13",
+  "samsung s23",
+  "samsung s20",
+];
+
+// 4-Drucken Sie alle Produkte in der Produktliste mit einem Großbuchstaben.
+
+// 5- Wie viele Produkte von Samsung sind in der Produktliste enthalten?
+
+let schüler = [
+  { name: "alex", nachname: "captain", not: [60, 70, 60] },
+  { name: "ryan", nachname: "müller", not: [80, 70, 80] },
+  { name: "mike", nachname: "scholz", not: [10, 20, 60] },
+];
+
+// 6- Drucken Sie den Notendurchschnitt und den Leistungsstatus jedes Schülers in der Schülerliste aus.
