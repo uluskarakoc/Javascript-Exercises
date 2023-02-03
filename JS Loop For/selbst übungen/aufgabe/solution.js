89317d84852120dbe1b84adeafe4cc5432563c3c
ulus karakoc <uluskk@gmail.com>
2023-02-03 10:55:14 +0100

    mentor aufgabe

---
+++ JS Loop For/selbst übungen/aufgabe/solution.js
@@ -0,0 +1,35 @@
+// ## EXERCISE 1
+// Write the code to execute a SUM between the number 12 and 20
+let sum =0;
+for(i=12;i<=20;i++){
+ sum+=i;
+//  sum++;
+}
+console.log(sum)
+
+// ## EXERCISE 2
+// Create a variable named X containing the number 12
+let x =12;
+
+// ## EXERCISE 3
+// Create a variable named name containing the string John Doe
+const name ="John Doe"
+// ## EXERCISE 4
+// Execute a DIFFERENCE between the number 12 and the variable X, which stores the value 12
+console.log(x-12);
+
+// ## EXERCISE 5
+// Create two variables: name1 and name 2. name1 is equal to john, name2 is equal to John.
+// Verify that name1 is different from name2. 
+// Verify then, that name1 and name2 are equals if both lowercase (without changing the value of name2)
+
+// ## EXERCISE 6
+// Create the variable X (value less than 10). Write the code to print the literal value of the given number (ex.: 1 => one, 5 => five)
+
+// ## EXERCISE 7 (EXTRA)
+// Insert a value in a variable based on the result of a ternary if
+
+
+
+
+
--- JS Loop For/selbst übungen/solution.js
+++ JS Loop For/selbst übungen/solution.js
@@ -1,25 +1,25 @@
 let zahlen = [1, 5, 7, 15, 3, 25, 12, 24];
 
 // 1- Schreiben Sie der Quadratwurzel jedes Element in der Zahlen?
-for (let zahl = 0; zahl < zahlen.length; zahl++){
-  console.log(`1---${zahlen[zahl]*zahlen[zahl]}`)
+for (let i = 0; i < zahlen.length; i++) {
+  console.log(`1---${zahlen[i] * zahlen[i]}`);
 }
 
-// for(let zahl of zahlen){
-//   console.log(zahl*zahl)
-// }
+for (let zahl of zahlen) {
+  console.log(zahl * zahl);
+}
 
 // 2- Welche Zahlen ist 5 mal große von 5 auf dem Zahlenlist
-for(let zahl=0; zahl<zahlen.length; zahl++){
-  if(zahlen[zahl] % 5==0){
-    console.log(`2---${zahlen[zahl]}`)
+for (let zahl = 0; zahl < zahlen.length; zahl++) {
+  if (zahlen[zahl] % 5 == 0) {
+    console.log(`2---${zahlen[zahl]}`);
   }
 }
 
 // 3- Finden Sie die summe der gerade Zahlen auf dem Zahlen list
-for(let ulus = 0; ulus<zahlen.length; ulus++){
-  if(zahlen[ulus] % 2 == 0){
-    console.log(`3---gerade zahlen sind ${zahlen[ulus]}`)
+for (let ulus = 0; ulus < zahlen.length; ulus++) {
+  if (zahlen[ulus] % 2 == 0) {
+    console.log(`3---gerade zahlen sind ${zahlen[ulus]}`);
   }
 }
 
@@ -32,12 +32,27 @@ let produkte = [
 ];
 
 // 4-Drucken Sie alle Produkte in der Produktliste mit einem Großbuchstaben.
-for (ulus=0; ulus<produkte.length; ulus++){
-  console.log(`4---${produkte[ulus].toUpperCase()}`)
+for (ulus = 0; ulus < produkte.length; ulus++) {
+  console.log(`4---${produkte[ulus].toUpperCase()}`);
 }
 
-
 // 5- Wie viele Produkte von Samsung sind in der Produktliste enthalten?
+
+// let arr =[];
+// for(i =0; i<produkte.length; i++){
+//   if(produkte[i].includes("samsung")){
+//     arr.push(produkte[i])
+//   }
+// }
+// console.log(arr.length)
+
+let stuck = 0;
+for (i = 0; i < produkte.length; i++) {
+  if (produkte[i].includes("samsung")) {
+    stuck++;
+  }
+}
+console.log(`5---${stuck}`);
 
 let schüler = [
   { name: "alex", nachname: "captain", not: [60, 70, 60] },
@@ -46,3 +61,11 @@ let schüler = [
 ];
 
 // 6- Drucken Sie den Notendurchschnitt und den Leistungsstatus jedes Schülers in der Schülerliste aus.
+for (
+  notendurchschnitt = 0;
+  notendurchschnitt < schüler.length;
+  notendurchschnitt++
+) {
+  console.log();
+}
+
