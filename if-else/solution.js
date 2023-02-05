631491b23218455597f1cf2a53528a1c374db7eb
ulus karakoc <uluskk@gmail.com>
2023-02-05 19:52:03 +0100

    if/else2

--- if-else/solution.js
+++ if-else/solution.js
@@ -2,11 +2,15 @@
 // 1-Kontrollieren sie ob ein irgendzahl zwischen 10-50 sind?
 let zahl =21;
 if(zahl<50 && zahl>10){
-    console.log(`${zahl} ist zwischen 10 und 50.`)
+    console.log(`1--${zahl} ist zwischen 10 und 50.`)
 }
 
 // 2 - Bir sayinin pozitif tek sayi olup olmadigini kontrol ediniz.
-// 2-Kontrollieren sie ob ein irgendzahl pozitif und ungerade zahl ist?
+// 2-Kontrollieren sie ob ein irgendzahl positiv und ungerade zahl ist?
+ zahl =21;
+ if(zahl%2 == 1 && zahl>0){
+    console.log(`2--${zahl} ist ungerade und positiv.`)
+} 
 
 // 3 - x, y, z sayilarinin büyüklük karsilastirilmasini yapiniz.(else if'i arastiriniz.)
 // 3-Vergleichen sie die zahlen, die x,y,z sind, als große?
