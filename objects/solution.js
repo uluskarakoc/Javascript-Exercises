f5f5b847775c713df824a89bfe7ddb5f643510b2
ulus karakoc <uluskk@gmail.com>
2023-02-06 21:30:32 +0100

    objects

--- objects/solution.js
+++ objects/solution.js
@@ -1,46 +1,35 @@
-/* 
-    1- Sipariş bilgilerini object içerisinde saklayınız.
-    2- Her siparişin ayrı ayrı kdv dahil toplam ödenen ücretini hesaplayınız. (kdv: %18)
-    3- Tüm siparişlerin kdv dahil toplam ödenen ücretini hesaplayınız.
+const ich = {
+    name:"ulus",
+    nachname:"karakoc",
+    alt:"32",
+    hobies:["schwimmen","movie"],
+  adresse: {
+    stadt: "Hamburg",
+    kreis: "harburg",
+    wohnort: "Buchholz",
+    straße: "Bremer",
+    nummer: "48",
+  },
+  books:["bücher","sonsuz nur"]
+};
 
-    sipariş id: 101
-    sipariş tarihi: 31.12.2022
-    ödeme şekli: kredi kartı
-    kargo adresi: Yahya kaptan mah. Kocaeli İzmit
-    satın alınan ürünler: 
-        ürün id: 5
-        ürün başlığı: IPhone 13 Pro
-        ürün url: http://abc.com/iphone-13-pro
-        ürün fiyatı: 22000
+const fatih = {
+    name:"ulus",
+    nachname:"karakoc",
+    alt:"32",
+    hobies:["schwimmen","movie"],
+  adresse: {
+    stadt: "Hamburg",
+    kreis: "harburg",
+    wohnort: "Buchholz",
+    straße: "Bremer",
+    nummer: "48",
+  },
+  books:["bücher","ali"]
+};
 
-        ürün id: 6
-        ürün başlığı: IPhone 13 Pro Max
-        ürün url: http://abc.com/iphone-13-pro-max
-        ürün fiyatı: 25000
 
-    müşteri:
-        müşteri id: 12
-        
-    satıcı:
-        firma id: 34
-        firma adı: Apple Türkiye
+let leute=[ich, fatih]
 
-    sipariş id: 102
-    sipariş tarihi: 30.12.2022
-    ödeme şekli: kredi kartı
-    kargo adresi: Yahya kaptan mah. Kocaeli İzmit
-    satın alınan ürünler: 
-
-        ürün id: 6
-        ürün başlığı: IPhone 13 Pro Max
-        ürün url: http://abc.com/iphone-13-pro-max
-        ürün fiyatı: 25000
-
-    müşteri:
-        müşteri id: 12
-        
-    satıcı:
-        firma id: 34
-        firma adı: Apple Türkiye
-
-*/
+console.log(ich.adresse.straße)
+console.log(ich.books[1])
