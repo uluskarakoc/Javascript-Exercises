2930a810db3d7125ddd7c5b5d3a67f8e601a1b11
ulus karakoc <uluskk@gmail.com>
2023-02-08 00:11:12 +0100

    function1

--- fonksiyonlar/solution.js
+++ fonksiyonlar/solution.js
@@ -12,5 +12,18 @@ function alan(a, b) {
 }
 alan(7, 8);
 // 3- Yazi tura uygulamasini fonksiyon kullanarak yapiniz.
+
+function para(sayi) {
+    if(sayi%2 == 0){
+        console.log("yazi")
+    }else{
+    
+        console.log("tura")
+    }
+}
+// console.log(para(Math.floor(Math.random() * 10)));
+para(Math.floor(Math.random() * 10))
+
+
 // 4-kendisine gönderilen bir sayinin tam bölenlerinini dizi sekline döndüren fonksiyonu yaziniz.
 // 5-Degisken sayida parametre alan toplam isminde bir fonksiyon tanimlayiniz.
