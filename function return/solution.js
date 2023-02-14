874fe84a3ae9482e23c7b06e90f8971897497b06
ulus karakoc <uluskk@gmail.com>
2023-02-14 11:17:05 +0100

    aufgaben weiter

--- function return/solution.js
+++ function return/solution.js
@@ -67,3 +67,48 @@ const isString = (a) => {
 };
 let result2=isString("ulus")
 console.log(result2)
+
+// 8. Write an arrow function named `areStrings` that takes in two parameters and returns true if both of them are strings.
+const areStrings = (a,b) => {
+  if (isNaN(a) &&(isNaN(b))) {
+    return true;
+  }
+};
+let result3=areStrings("ulus","karakoc")
+console.log(result3)
+
+// 9. Write an arrow function named `getFirstWord` that takes in one string as parameter. The function should return the first word in the string. Examples:
+//     - getFirstWord("Internetting is hard") --> "Internetting"
+//     - getFirstWord("Hello World") --> "Hello"
+//     - getFirstWord("Hello") --> "Hello"
+name1="ulus karakoc"
+const getFirstWord =(name1)=>{
+return name1
+}
+let result4 =getFirstWord()
+console.log(result4)
+
+// 10. *HARD* Write an arrow function named `wordRepeater` that takes in one string as parameter.
+// The function should return a new string where each word in the input string is repeated by the number of words in the input string. Examples:
+//     - wordRepeater("bunny") --> "bunny"
+//     - wordRepeater("Cat food") --> "Cat Cat food food"
+//     - wordRepeater("I am groot") --> "I I I am am am groot groot groot"
+//     - wordRepeater("O M G ?") --> "O O O O M M M M G G G G ? ? ? ?"
+
+
+
+// 11. Write a short (single line) arrow function named `firstLetter` that takes in one string and returns the first character of that string. Examples:
+//     - firstLetter("cat") --> "c"
+//     - firstLetter("quylthulg") --> "q"
+const name2 ="ulus karakoc"
+const firstLetter =(a)=>{
+  return name2[a]
+}
+const result5 = firstLetter(0)
+console.log(result5)
+
+// 12. Schreibe eine Pfeilfunktion namens `firstLetters`, die eine Zeichenkette als Parameter entgegennimmt und eine neue Zeichenkette zurückgibt. Die neue Zeichenkette muss aus dem ersten Buchstaben jedes Wortes in der Eingabe gebildet werden. Beispiele:
+//     - firstLetters("Cat") --> C"
+//     - firstLetters("What the fruit") --> "Wtf"
+//     - firstLetters("MongoDB Express Node React") --> "MERN"
+//     - firstLetters("What You See Is What You Get") --> "WYSIWYG"
