7b4b40d71e5677eb1ab3e2546cd0e7db38eb9952
ulus karakoc <uluskk@gmail.com>
2023-02-16 21:25:58 +0100

    function

--- functions-revisit-bmi-calculator/solution.js
+++ functions-revisit-bmi-calculator/solution.js
@@ -9,6 +9,14 @@ console.log(calculateBMI(80,175).toFixed(2))
 console.log(calculateBMI().toFixed(2))
 // ## 2. BMI vergleichen
 result()
-const whoIsBiggest=(name="ulus",name2="serhat",ulusboy=175,serboy=190)=>{
-    return 
+const whoIsBiggest=(name1,name2,große1,große2)=>{
+    if(große1>große2){
+        console.log(`${name1} ist großer`);
+        return name1
+    }else{
+        console.log(`${name2} ist großer`)
+        return name2
+    }
+   
 }
+console.log(whoIsBiggest("Frank", "Mark", 26.72, 28.33))
