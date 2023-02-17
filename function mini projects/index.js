462718de24882eeb2196906bd57eefda61ae1471
ulus karakoc <uluskk@gmail.com>
2023-02-17 22:48:14 +0100

    hundejahr function

--- function mini projects/index.js
+++ function mini projects/index.js
@@ -2,16 +2,27 @@ let i = 1;
 function result() {
   console.log(`Result ${i++}`);
 }
-result();
+
 // The Fortune Teller
-const tellFortune = (kinder=4, partner="yulia", standort="Hamburg", beruf="webdeveloper") => {
+result();
+const tellFortune = (
+  kinder = 4,
+  partner = "yulia",
+  standort = "Hamburg",
+  beruf = "webdeveloper"
+) => {
   return `Du wirst ein ${beruf} in ${standort} sein, verheiratet mit ${partner} und hast ${kinder} Kinder.`;
 };
 console.log(tellFortune());
-console.log(tellFortune('double', 'Japan', 'Los angelas', "actress"));
-
-
+console.log(tellFortune("double", "Japan", "Los angelas", "actress"));
 // The Puppy Age Calculator
+result();
+const calculateDogAge = (menschenjahr) => {
+  return `Dein Hündchen ist ${menschenjahr * 7} Jahre alt in Hundejahren!`;
+};
+console.log(calculateDogAge(3));
+console.log(calculateDogAge(2));
+console.log(calculateDogAge(9));
 
 // The Lifetime Supply Calculator
 
