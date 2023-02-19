b7a4c2264a0551caa4233f3421dd2234385b3f8d
ulus karakoc <uluskk@gmail.com>
2023-02-19 22:33:54 +0100

    slice

--- Mentor exercise 1/EX2.md
+++ Mentor exercise 1/EX2.md
@@ -2,28 +2,20 @@
 ## Write a function "area" which receives 2 parameters (l1,l2) and calculate the area of the rectangle.
 
 
-
 # EXERCISE 2
 ## Write a function "crazySum" which receives two given integers. If the two values are same, then returns triple their sum.
-
 
 
 # EXERCISE 3
 ## Write a function "crazyDiff" that computes the absolute difference between a given number and 19. Returns triple their absolute difference if the specified number is greater than 19.
 
 
-
-
 # EXERCISE 4
 ## Write a function "strivify" which accepts a string S. Add to S "Strive" in front of a given string, if the given string begins with "Strive" then return the original string.
 
 
-
-
 # EXERCISE 5
 ## Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7. HINT: u
-
-
 
 
 # EXERCISE 6
--- Mentor exercise 1/solution.js
+++ Mentor exercise 1/solution.js
@@ -40,20 +40,22 @@ const strivify = (s) => {
 console.log(strivify("ulus"));
 // # EXERCISE 5
 result();
-const check3and7=(n)=>{
-  if(n%3===0){
-    return true
-  }else if(n%7===0){
-    return `2 mal ${true}`
-  }else{
-    return `sayisinin kendisi ${n}`
+const check3and7 = (n) => {
+  if (n % 3 === 0) {
+    return true;
+  } else if (n % 7 === 0) {
+    return `2 mal ${true}`;
+  } else {
+    return `sayisinin kendisi ${n}`;
   }
-}
-console.log(check3and7(28))
+};
+console.log(check3and7(28));
 // # EXERCISE 6
 result();
-const reverseString=(n)=>{return n.split("").reverse().join("")} 
-console.log(reverseString("ulus"))
+const reverseString = (n) => {
+  return n.split("").reverse().join("");
+};
+console.log(reverseString("ulus"));
 // # EXERCISE 7
 result();
 // const upperFirst=(n)=>{return n.charAt(0).toUpperCase() + n.slice(1)}
@@ -62,10 +64,14 @@ result();
 function upperFirst(s) {
   let arr = s.split(" ");
   for (let i = 0; i < arr.length; i++) {
-      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
   }
   return arr.join(" ");
 }
-
-console.log(upperFirst("ulus karakoc konyada yasiyorum"))
-
+console.log(upperFirst("ulus karakoc konyada yasiyorum"));
+// # EXERCISE 8
+result();
+const cutString = (n) => {
+  return n.slice(1, n.length - 1);
+};
+console.log(cutString("buchholz"));
