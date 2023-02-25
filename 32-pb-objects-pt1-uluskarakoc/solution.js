2482cd25992d880bb2b8d502a95a53ed4c555a28
ulus karakoc <uluskk@gmail.com>
2023-02-25 21:01:47 +0100

    aufgabe1

--- 32-pb-objects-pt1-uluskarakoc/solution.js
+++ 32-pb-objects-pt1-uluskarakoc/solution.js
@@ -1,12 +1,39 @@
+let i = 1;
+const result = () => {
+  console.log(`Result*****${i++}`);
+};
+
 /**
  * Implement your solutions here
  */
 
 // 1. Get Properties
+result();
+const getProperties = {
+  name: "Mike",
+  class: "4A",
+  course: "English",
+};
+const keys = (obj) => {
+  return Object.keys(obj);
+};
+console.log(keys(getProperties));
 
 // 2. Get Values
-
+result();
+const values = { choice1: "tea", choice2: "coffee", choice3: "milk" };
+const getValues=(v)=>{
+    return Object.values(v)
+}
+console.log(getValues(values))
 // 3. Convert Keys and Values into arrays
+result();
+const KeyValuePairs={ cats: 1, dogs: 2, turtles: 4 }
+const getKeyValuePairs=(o)=>{
+    return Object.entries(o)
+}
+console.log(getKeyValuePairs(KeyValuePairs))
+
 
 // 4. Convert keys and values into objects
 
