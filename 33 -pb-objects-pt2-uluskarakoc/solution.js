9b53f46d8c5b839be578ca30b55d85b397035531
ulus karakoc <uluskk@gmail.com>
2023-02-26 23:34:41 +0100

    aufgabe8

--- 33 -pb-objects-pt2-uluskarakoc/solution.js
+++ 33 -pb-objects-pt2-uluskarakoc/solution.js
@@ -1,7 +1,12 @@
 let i=1;
 const result=()=>{console.log(`Result*****${i++}`)}
-result()
 
 // **1. Get properties.**
+result()
 const getProperties=(obj)=>{return Object.keys(obj)}
 console.log(getProperties({ name: "Mike", class: "4A", course: "English" }))
+// **2. Get values.**
+result()
+
+const getValues=(obj)=>{return Object.values(obj)}
+console.log(getValues({ choice1: "tea", choice2: "coffee", choice3: "milk" }));
