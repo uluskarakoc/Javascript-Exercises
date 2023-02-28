a19b15c40de5db393a61176f42a116a43b20d4af
ulus karakoc <uluskk@gmail.com>
2023-02-28 10:19:27 +0100

    spread2

--- 35-pb-datastructure-spread-uluskarakoc/index.js
+++ 35-pb-datastructure-spread-uluskarakoc/index.js
@@ -1,15 +1,31 @@
-let i=1;
-const start =()=>{console.log(`Result*****${i++}`);}
-const end =()=>{console.log("-".repeat(20));}
-start()
+let i = 1;
+const start = () => {
+  console.log(`Result*****${i++}`);
+};
+const end = () => {
+  console.log("-".repeat(20));
+};
+start();
 // #### 1. Kombinieren von Arrays
-let euroCountries=["Germany","France"]
-let asianCountries=["Japan","Korea"]
-let arr=euroCountries.push(asianCountries)
+let euroCountries = ["Germany", "France"];
+let asianCountries = ["Japan", "Korea"];
+let arr = euroCountries.push(asianCountries);
 console.log(euroCountries);
-let euroCities=["Berlin","Paris"]
-let asiaCities=["Tokyo","Seoul"]
-let worldCities=[...euroCities,...asiaCities]
+let euroCities = ["Berlin", "Paris"];
+let asiaCities = ["Tokyo", "Seoul"];
+let worldCities = [...euroCities, ...asiaCities];
 console.log(worldCities);
-
-end()
+end();
+start();
+// #### 2. Copying Arrays
+let newArr = [...asiaCities];
+console.log(newArr);
+end();
+// #### 3. Find the Largest
+start();
+let number = [1, 4, 45, 80, 300];
+const findLargestNumber = (arr) => {
+  return Math.max(...arr);
+};
+console.log(findLargestNumber(number));
+end();
