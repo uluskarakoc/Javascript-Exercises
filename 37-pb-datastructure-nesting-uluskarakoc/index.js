74747ee9b3049148304e9a0e13700c1130a37592
ulus karakoc <uluskk@gmail.com>
2023-02-28 16:50:03 +0100

    nested1

--- 37-pb-datastructure-nesting-uluskarakoc/index.js
+++ 37-pb-datastructure-nesting-uluskarakoc/index.js
@@ -1,17 +1,40 @@
 let i = 1;
 const result = () => {
-  console.log(`Result***********${i++}`);}
+  console.log(`Result***********${i++}`);
+};
 // #### 1. 2D Array
-result()
+result();
 let board = [
-    [1, 2, 3],
-    ["quick", "brown", "fox", "jumped", "over", "lazy", "dog"],
-    [true, false]
-  ];
-  for(i=0;i<board.length;i++){
-    // console.log(board[i])
-  for(j=0;j<board[i].length;j++){
-    console.log(board[i][j])
+  [1, 2, 3],
+  ["quick", "brown", "fox", "jumped", "over", "lazy", "dog"],
+  [true, false],
+];
+for (let i = 0; i < board.length; i++) {
+  // console.log(board[i])
+  let sum = "";
+  for (j = 0; j < board[i].length; j++) {
+    // console.log(board[i][j]);
+    sum += board[i][j];
   }
+  console.log(sum);
+}
+// #### 2. Doggo
+result();
+// 2.1
+const doggo = { name: "lion", breed: "Kangal" };
+// 2.2
+doggo.favoriteFoods = ["fleisch", "ice", "chicken"];
+console.log(doggo);
+// 2.3
+doggo.favoriteFoods[1];
+// console.log(doggo.favoriteFoods[1])
+// 2.4
+doggo.showFavorites = () => {
+  let sum = "";
+  for (let i = 0; i < doggo.favoriteFoods.length; i++) {
+    sum += doggo.favoriteFoods[i];
   }
-  
+  console.log(sum);
+};
+// console.log(doggo.showFavorites())
+doggo.showFavorites();
