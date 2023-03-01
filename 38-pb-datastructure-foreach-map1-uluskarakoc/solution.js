b4d6f867fe628b20cf5bf99c90262d1db503fd5b
ulus karakoc <uluskk@gmail.com>
2023-03-01 14:45:35 +0100

    map2

--- 38-pb-datastructure-foreach-map1-uluskarakoc/solution.js
+++ 38-pb-datastructure-foreach-map1-uluskarakoc/solution.js
@@ -1,10 +1,31 @@
-let i=1;
-const result =()=>{console.log(`Result💥💥💥💥💥💥${i++}`)}
-result()
+let i = 1;
+const result = () => {
+  console.log(`Result💥💥💥💥💥💥${i++}`);
+};
+result();
 
 // **1.** Write a function named `doubleValues` which accepts an array and returns a new array with all the original values doubled.
-const doubleValues=(arr)=>{
-console.log(arr.map((n)=>n*2))
-}
-doubleValues([1,2,3]) // [2,4,6]
-doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
+const doubleValues = (arr) => {
+  console.log(arr.map((n) => n * 2));
+};
+doubleValues([1, 2, 3]); // [2,4,6]
+doubleValues([5, 1, 2, 3, 10]); // [10,2,4,6,20]
+
+// **2.** Write a function named `onlyEvenValues` which accepts an array and returns a new array with only the even values from the original array.
+result();
+const onlyEvenValues = (arr) => {
+  const filteredArray = arr.map((n) => {
+    if (n % 2 === 0) {
+      return n;
+    } else {
+      return [];
+    }
+  });
+  return filteredArray.flat().flat()
+};
+
+console.log(onlyEvenValues([1, 2, 3]));
+console.log(onlyEvenValues([5, 1, 2, 3, 10]));
+
+// **3.** Write a function named `showFirstAndLast` which accepts an array of strings and returns a new array with only the first and last character of each string.
+result();
