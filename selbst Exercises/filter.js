e1b109fc9a3e1c22cdc615abde11511a6073c957
ulus karakoc <uluskk@gmail.com>
2023-03-04 18:33:47 +0100

    filter

---
+++ selbst Exercises/filter.js
@@ -0,0 +1,28 @@
+let i=1;
+const result=()=>{console.log(`Result🥊🥊🥊🥊🥊🥊${i++}`)}
+// The following example returns all prime numbers in the array:
+result()
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+function isPrime(num) {
+  for (let i = 2; num > i; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+result()
+const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+
+/**
+ * Filter array items based on search criteria (query)
+ */
+function filterItems(arr, query) {
+  return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+
+console.log(filterItems(fruits, "ap")); // ['apple', 'grapes']
+console.log(filterItems(fruits, "an")); // ['banana', 'mango', 'orange']
+
