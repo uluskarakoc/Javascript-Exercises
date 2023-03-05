644bd4b403d032e2bd3751f6c5fd8de3842f2283
ulus karakoc <uluskk@gmail.com>
2023-03-05 21:58:18 +0100

    camelcase1

--- 41-pb-datastructure-arraymethod-filter-uluskarakoc /index.js
+++ 41-pb-datastructure-arraymethod-filter-uluskarakoc /index.js
@@ -1,25 +1,35 @@
+let i = 1;
+const result = () => {
+  console.log(`Result--------------${i++}`);
+};
 //Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
 //1
-
-function filterRange(arr, a, b) {
-  //write your code here
-}
-
-let arr = //[5, 3, 8, 1];
-
+result()
+let arr = [5, 3, 8, 1];
 let filtered = filterRange(arr, 1, 4);
+function filterRange(arr, a, b) {
+  return arr.filter(num => num>=a && num<=b)
+}
+ console.log( filtered ); 
+//  console.log( arr ); 
 
-console.log( filtered ); // 3,1 (matching values)
 
-console.log( arr ); // 5,3,8,1 (not modified)
+
 
 //Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 //2
+result()
 function filterRangeInPlace(arr, a, b) {
-  //write your code here
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1); 
+      i--; 
+    }
+  }
 }
 
-let array = //[5, 3, 8, 1];
+
+let array = [5, 3, 8, 1];
 
 filterRangeInPlace(array, 1, 4); // removed the numbers except from 1 to 4
 
