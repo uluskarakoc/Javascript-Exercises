1498113013c41d0068736361ad2aec6c8cbc74a0
ulus karakoc <uluskk@gmail.com>
2023-03-07 23:35:48 +0100

    sort2

--- 45- pb-datastructure-arraymethod-sort-uluskarakoc/index.js
+++ 45- pb-datastructure-arraymethod-sort-uluskarakoc/index.js
@@ -7,7 +7,7 @@ result();
 //1
 let arr = [5, 2, 1, -10, 8];
 
-console.log(arr.sort((a,b)=>b-a));
+console.log(arr.sort((a, b) => b - a));
 
 //We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
 //Create a function copySorted(arr) that returns such a copy.
@@ -15,11 +15,12 @@ console.log(arr.sort((a,b)=>b-a));
 //2
 //We can use slice() to make a copy and run the sort on it:
 result();
+const copySorted = (arr) => {
+ return arr.sort((a, b) => a.localeCompare(b));
+};
+let array = ["HTML", "JavaScript", "CSS"];
 
+let sorted = copySorted(array);
 
-// let array = ["HTML", "JavaScript", "CSS"];
-
-// let sorted = copySorted(array);
-
-// console.log(sorted);
-// console.log(array);
+console.log(sorted);
+console.log(array);
