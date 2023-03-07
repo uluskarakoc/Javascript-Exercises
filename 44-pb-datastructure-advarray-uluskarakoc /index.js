b90d54aa5c25d2fc5a1fa24aa623484bbf542d3e
ulus karakoc <uluskk@gmail.com>
2023-03-07 11:08:38 +0100

    reduce1

--- 44-pb-datastructure-advarray-uluskarakoc /index.js
+++ 44-pb-datastructure-advarray-uluskarakoc /index.js
@@ -11,15 +11,28 @@ const orders = [
   { amount: 325 },
 ];
 const bestellungen = orders.reduce((acc, curr) => {
-  return acc+curr.amount
+  return acc + curr.amount;
 }, 0);
-console.log(bestellungen)
+console.log(bestellungen);
 // ##### 2. Inkrementieren um 1
 result();
-const arrayOfNumbers = [3, 45, 6, 56, 7, 9]; 
-const erhöhen=arrayOfNumbers.map((n)=> n+1)
-console.log(erhöhen)
+const arrayOfNumbers = [3, 45, 6, 56, 7, 9];
+const erhöhen = arrayOfNumbers.map((n) => n + 1);
+console.log(erhöhen);
 // #### 3. Evens filtern
-result()
-const gerade=[1,2,3,11,12,13].filter((n)=> n%2===0)
-console.log(gerade)
+result();
+const gerade = [1, 2, 3, 11, 12, 13].filter((n) => n % 2 === 0);
+console.log(gerade);
+// #### 4. Freunde filtern
+result();
+// #### 5. Summe
+result();
+const summieren=[1,2,3,4,5].reduce((acc,curr)=> {return acc +curr},0)
+const summieren1=[6,7,7].reduce((acc,curr)=> {return acc +curr},0)
+console.log(summieren)
+console.log(summieren1)
+// #### 6. Quadratwurzel
+const friends = ["rika", "jenna", "bleda", "oliver", "itamar"];
+
+
+
