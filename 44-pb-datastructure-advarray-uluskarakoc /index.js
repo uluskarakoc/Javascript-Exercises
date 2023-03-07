3f08f625be59fae824b095edc625d689bec1c4c1
ulus karakoc <uluskk@gmail.com>
2023-03-07 10:57:35 +0100

    map

--- 44-pb-datastructure-advarray-uluskarakoc /index.js
+++ 44-pb-datastructure-advarray-uluskarakoc /index.js
@@ -14,3 +14,9 @@ const bestellungen = orders.reduce((acc, curr) => {
   return acc+curr.amount
 }, 0);
 console.log(bestellungen)
+// ##### 2. Inkrementieren um 1
+result();
+const arrayOfNumbers = [3, 45, 6, 56, 7, 9]; 
+const erhöhen=arrayOfNumbers.map((n)=> n+1)
+console.log(erhöhen)
+
