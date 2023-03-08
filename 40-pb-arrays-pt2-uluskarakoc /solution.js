6d3e5d4d7593adac40f7c5a5c5bbb2e0a93aef85
ulus karakoc <uluskk@gmail.com>
2023-03-08 14:25:27 +0100

    array pt4

--- 40-pb-arrays-pt2-uluskarakoc /solution.js
+++ 40-pb-arrays-pt2-uluskarakoc /solution.js
@@ -21,17 +21,42 @@ const findGreatest = (arr, num) => {
 console.log(findGreatest([3, 4, 5], 4));
 console.log(findGreatest([10, 20, 30], 12));
 console.log(findGreatest([0, 10, 3], 4));
-result();
 // **2. The longest word.**
+result();
 const getLongestWord = (str) => {
   let longstword = "";
-    str.split(" ").forEach((n) => {
-      if (n.length > longstword.length) {
-        longstword = n;
-      }
-    })
-  ;
+  str.split(" ").forEach((n) => {
+    if (n.length > longstword.length) {
+      longstword = n;
+    }
+  });
   return longstword;
 };
-;
 console.log(getLongestWord("this is a web development course"));
+// **3. Reverse.**
+result();
+const reverseNum = (num) => {
+  let result1 = parseInt(num.toString().split("").reverse().join(""));
+  if (num < 0) {
+    result1 = -result1;
+  }
+  return result1;
+};
+console.log(reverseNum(34532));
+console.log(reverseNum(-34532));
+// **4. "AEIOU": Vowels.**
+result();
+const vowelCounter = (str) => {
+  const vowels = ["a", "e", "i", "o", "u"]
+  let count = 0;
+  str.split("").forEach((n) => {
+    if (vowels.includes(n)) {
+      count++;
+    }
+  });
+console.log(count)
+};
+vowelCounter("this is a string");
+vowelCounter("ulus karakoc");
+
+// console.log(("this is a string").split(""))
