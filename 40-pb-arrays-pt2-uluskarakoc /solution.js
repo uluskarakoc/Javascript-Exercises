4862dc6a81f382f7c3f02ca8ae1fe1f3fe92d23d
ulus karakoc <uluskk@gmail.com>
2023-03-08 11:45:26 +0100

    array pt2

--- 40-pb-arrays-pt2-uluskarakoc /solution.js
+++ 40-pb-arrays-pt2-uluskarakoc /solution.js
@@ -1,1 +1,16 @@
-
+let i = 1;
+const result = () => {
+  console.log(`Result---------------${i++}`);
+};
+result();
+// **1. Die Größeren Zahlen.**
+const findGreatest = (arr, num) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (num < arr[i]) {
+      console.log(arr[i]);
+    }
+  }
+};
+findGreatest([3, 4, 5], 4);
+findGreatest([10, 20, 30], 12);
+findGreatest([0, 10, 3], 4);
