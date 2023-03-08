f0e57ab21f90dd2b49b98ab91641449e67cdc92f
ulus karakoc <uluskk@gmail.com>
2023-03-08 22:08:23 +0100

    parseInt

--- 40-pb-arrays-pt2-uluskarakoc /solution.js
+++ 40-pb-arrays-pt2-uluskarakoc /solution.js
@@ -4,23 +4,26 @@ const result = () => {
 };
 result();
 // **1. Die Größeren Zahlen.**
-// const findGreatest = (arr, num) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (num < arr[i]) {
-//       console.log(arr[i]);
-//     }
-//   }
-// };
-// findGreatest([3, 4, 5], 4);
-// findGreatest([10, 20, 30], 12);
-// findGreatest([0, 10, 3], 4);
-
 const findGreatest = (arr, num) => {
-  return arr.filter((n) => n > num).join(" ");
+  let array = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (num < arr[i]) {
+      array.push(arr[i]);
+    }
+  }
+  return array.join(" ");
 };
+findGreatest([3, 4, 5], 4);
+findGreatest([10, 20, 30], 12);
+findGreatest([0, 10, 3], 4);
+
+// const findGreatest = (arr, num) => {
+//   return arr.filter((n) => n > num).join(" ");
+// };
 console.log(findGreatest([3, 4, 5], 4));
 console.log(findGreatest([10, 20, 30], 12));
 console.log(findGreatest([0, 10, 3], 4));
+
 // **2. The longest word.**
 result();
 const getLongestWord = (str) => {
@@ -33,6 +36,7 @@ const getLongestWord = (str) => {
   return longstword;
 };
 console.log(getLongestWord("this is a web development course"));
+
 // **3. Reverse.**
 result();
 const reverseNum = (num) => {
@@ -44,6 +48,7 @@ const reverseNum = (num) => {
 };
 console.log(reverseNum(34532));
 console.log(reverseNum(-34532));
+
 // **4. "AEIOU": Vowels.**
 result();
 const vowelCounter = (str) => {
@@ -63,13 +68,19 @@ vowelCounter("ulus karakoc");
 result();
 const findMissingNum = (arr) => {
   let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  arr.forEach((n) => {
-    if (!number.includes(n)) {
-      return n;
+  let missing = [];
+  number.forEach((n) => {
+    if (!arr.includes(n)) {
+      missing.push(n);
     }
   });
+  return missing;
 };
 console.log(findMissingNum([1, 2, 3, 4, 6, 7, 8, 9, 10]));
+
+
+
+
 // **6. Cubed.**
 result();
 const sumOfCubes = (arr) => {
@@ -82,6 +93,11 @@ const sumOfCubes = (arr) => {
 console.log(sumOfCubes([1, 5, 9]));
 console.log(sumOfCubes([2]));
 console.log(sumOfCubes([]));
+
+
+
+
+
 // **7. Wörterbuch.**
 result();
 const dictionary = (str, arr) => {
@@ -90,6 +106,12 @@ const dictionary = (str, arr) => {
 console.log(dictionary("bu", ["button", "breakfast", "border"]));
 console.log(dictionary("tri", ["triplet", "tries", "trip", "piano", "tree"]));
 console.log(dictionary("beau", ["pastry", "delicious", "name", "boring"]));
+
+
+
+
+
+
 // **8. Even Number Generator.**
 result();
 const getEvenNums = (num) => {
@@ -99,16 +121,20 @@ const getEvenNums = (num) => {
   }
   return arr;
 };
-console.log(getEvenNums(8))
-console.log(getEvenNums(4))
-console.log(getEvenNums(2))
+console.log(getEvenNums(8));
+console.log(getEvenNums(4));
+console.log(getEvenNums(2));
+
+
+
+
+
 // **9: Alphabetische Reihenfolge**.
 result();
-const alphabetise=(str)=>{
-return str.split("").sort((a,b)=>a.localeCompare(b)).join("")
-}
+const alphabetise = (str) => {
+  return str
+    .split("")
+    .sort((a, b) => a.localeCompare(b))
+    .join("");
+};
 console.log(alphabetise("webdev"));
-
-
-
-
--- F.Krc-Execises/2/solution.js
+++ F.Krc-Execises/2/solution.js
@@ -1,133 +1,148 @@
-let i=1;
-const result=()=>{console.log(`Result----------------${i++}`)}
+let i = 1;
+const result = () => {
+  console.log(`Result----------------${i++}`);
+};
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
+  {
+    name: "Luke Skywalker",
+    height: 172,
+    mass: 77,
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: 202,
+    mass: 136,
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: 150,
+    mass: 49,
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: 188,
+    mass: 84,
+    eye_color: "blue",
+    gender: "male",
+  },
 ];
 //***MAP***
 //1. Get array of all names
-result()
-const allNames =characters.map(n=>{return n.name})
-console.log(allNames)
+result();
+const allNames = characters.map((n) => {
+  return n.name;
+});
+console.log(allNames);
 //2. Get array of all heights
-result()
-const allHeights =characters.map(n=>n.height).sort((a,b)=>a-b)
-console.log(allHeights)
+result();
+const allHeights = characters.map((n) => n.height).sort((a, b) => a - b);
+console.log(allHeights);
 //3. Get array of objects with just name and height properties
-result()
-const heightAndNames =characters.map((n)=>{ return n.name +" "+ n.height})
-console.log(heightAndNames)
+result();
+const heightAndNames = characters.map((n) => {
+  return n.name + " " + n.height;
+});
+console.log(heightAndNames);
 //4. Get array of all first names
-result()
-const firstNames =characters.map((n)=>{return n.name.slice(0,6)})
-console.log(firstNames)
+result();
+const firstNames = characters.map((n) => {
+  return n.name.slice(0, 6);
+});
+console.log(firstNames);
 //***FILTER***
 //1. Get characters with mass greater than 100
-result()
-const mass=characters.filter(n=>n.mass>100)
+result();
+const mass = characters.filter((n) => n.mass > 100);
 console.log(mass);
 //2. Get characters with height less than 200
-result()
-const height=characters.filter(n=>n.height<200)
+result();
+const height = characters.filter((n) => n.height < 200);
 console.log(height);
 //3. Get all male characters
-result()
-const male=characters.filter(n=>n.gender==="male")
+result();
+const male = characters.filter((n) => n.gender === "male");
 console.log(male);
 //4. Get all female characters
-result()
-const female=characters.filter(n=>n.gender==="female")
+result();
+const female = characters.filter((n) => n.gender === "female");
 console.log(female);
 //***SORT***
 //1. Sort by mass
-result()
-const massSort=characters.map(n=>n.mass).sort((a,b)=>a-b)
-console.log(massSort)
+result();
+const massSort = characters.map((n) => n.mass).sort((a, b) => a - b);
+console.log(massSort);
 //2. Sort by height
-result()
-const heightSort=characters.map((n)=>n.height).sort((a,b)=>a-b)
+result();
+const heightSort = characters.map((n) => n.height).sort((a, b) => a - b);
 console.log(heightSort);
 //3. Sort by name
-result()
-const nameSort=characters.map((n)=>n.name).sort((a,b)=>a.localeCompare(b))
+result();
+const nameSort = characters
+  .map((n) => n.name)
+  .sort((a, b) => a.localeCompare(b));
 console.log(nameSort);
 //4. Sort by gender
-result()
-const genderSort=characters.map((n)=>n.gender).sort((a,b)=>a.localeCompare(b))
+result();
+const genderSort = characters
+  .map((n) => n.gender)
+  .sort((a, b) => a.localeCompare(b));
 console.log(genderSort);
 //***SOME***
 //1. Is there at least one male character?
-result()
-const maleSome=characters.some(n=>n.gender==="male")
+result();
+const maleSome = characters.some((n) => n.gender === "male");
 console.log(maleSome);
 //2. Is there at least one character with blue eyes?
-result()
-const blueEyes=characters.some(n=>n.eye_color==="blue")
+result();
+const blueEyes = characters.some((n) => n.eye_color === "blue");
 console.log(blueEyes);
 //3. Is there at least one character taller than 210?
-result()
-const taller=characters.some(n=>n.height>210)
+result();
+const taller = characters.some((n) => n.height > 210);
 console.log(taller);
 //4. Is there at least one character that has mass less than 50?
-result()
-const massSome=characters.some(n=>n.mass<50)
+result();
+const massSome = characters.some((n) => n.mass < 50);
 console.log(massSome);
 //***EVERY***
 //1. Does every character have blue eyes?
-result()
-const blueEyesEvery=characters.every(n=>n.eye_color==="blue")
-console.log(blueEyesEvery)
+result();
+const blueEyesEvery = characters.every((n) => n.eye_color === "blue");
+console.log(blueEyesEvery);
 //2. Does every character have mass more than 40?
-result()
-const massEvery=characters.every(n=>n.mass>40)
-console.log(massEvery)
+result();
+const massEvery = characters.every((n) => n.mass > 40);
+console.log(massEvery);
 //3. Is every character shorter than 200?
-result()
-const heightEvery=characters.every(n=>n.height<200)
-console.log(heightEvery)
+result();
+const heightEvery = characters.every((n) => n.height < 200);
+console.log(heightEvery);
 //4. Is every character male?
-result()
-const genderEvery=characters.every(n=>n.gender==="male")
-console.log(genderEvery)
+result();
+const genderEvery = characters.every((n) => n.gender === "male");
+console.log(genderEvery);
 //***REDUCE***
-result()
+result();
 
 //1. Get total mass of all characters
-const massReduce=characters.reduce((acc,curr)=>{
-    return acc+curr.mass;
-},0)
+const massReduce = characters.reduce((acc, curr) => {
+  return acc + curr.mass;
+}, 0);
 console.log(massReduce);
 //2. Get total height of all characters
-result()
-const heightReduce=characters.reduce((acc,curr)=>{
-    return acc+curr.height;
-},0)
-console.log(he);
+result();
+const heightReduce = characters.reduce((acc, curr) => {
+  return acc + curr.height;
+}, 0);
+console.log(heightReduce);
 //3. Get total number of characters by eye color
+result();
+const eyeReduce = characters.map((n) => n.eye_color).reduce((a,c)=>{return a + c}).split(" ")
+console.log(eyeReduce);
 //4. Get total number of characters in all the character names
