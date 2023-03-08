ec464ab37b08da51adae8cfc1ab7c0cc44ee091a
ulus karakoc <uluskk@gmail.com>
2023-03-08 12:27:46 +0100

    array pt3

--- 40-pb-arrays-pt2-uluskarakoc /solution.js
+++ 40-pb-arrays-pt2-uluskarakoc /solution.js
@@ -4,13 +4,34 @@ const result = () => {
 };
 result();
 // **1. Die Größeren Zahlen.**
+// const findGreatest = (arr, num) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (num < arr[i]) {
+//       console.log(arr[i]);
+//     }
+//   }
+// };
+// findGreatest([3, 4, 5], 4);
+// findGreatest([10, 20, 30], 12);
+// findGreatest([0, 10, 3], 4);
+
 const findGreatest = (arr, num) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (num < arr[i]) {
-      console.log(arr[i]);
-    }
-  }
+  return arr.filter((n) => n > num).join(" ");
 };
-findGreatest([3, 4, 5], 4);
-findGreatest([10, 20, 30], 12);
-findGreatest([0, 10, 3], 4);
+console.log(findGreatest([3, 4, 5], 4));
+console.log(findGreatest([10, 20, 30], 12));
+console.log(findGreatest([0, 10, 3], 4));
+result();
+// **2. The longest word.**
+const getLongestWord = (str) => {
+  let longstword = "";
+    str.split(" ").forEach((n) => {
+      if (n.length > longstword.length) {
+        longstword = n;
+      }
+    })
+  ;
+  return longstword;
+};
+;
+console.log(getLongestWord("this is a web development course"));
