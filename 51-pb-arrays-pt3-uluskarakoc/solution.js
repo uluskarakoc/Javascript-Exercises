87740bf3c804d7695df108163e75be7ba190251d
ulus karakoc <uluskk@gmail.com>
2023-03-14 13:39:38 +0100

    array wiederholung3

--- 51-pb-arrays-pt3-uluskarakoc/solution.js
+++ 51-pb-arrays-pt3-uluskarakoc/solution.js
@@ -3,13 +3,37 @@ let i = 1;
 const result = () => {
   console.log(`Result----------${i++}`);
 };
+
 // **1. Where Have My Four Letter Words Gone?**
 result();
 const isFourLetters = (arr) => {
- return  arr.filter((n) => n.length === 4);
+  return arr.filter((n) => n.length === 4);
 };
+console.log(isFourLetters(["John", "James", "Jack", "Jeanne"]));
+console.log(isFourLetters(["Tomato", "Corn", "Lettuce"]));
+console.log(isFourLetters(["Dog", "Cat", "Deer"]));
+// **2. Print Month Name.**
+result();
 
-console.log(isFourLetters(["John", "James", "Jack", "Jeanne"]) );
-console.log( isFourLetters(["Tomato", "Corn", "Lettuce"]) );
- console.log(isFourLetters(["Dog", "Cat", "Deer"]));
-  
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const getMonthName = (num) => {
+  console.log(months[num -1]);
+};
+getMonthName(3);
+getMonthName(12);
+getMonthName(6);
+
