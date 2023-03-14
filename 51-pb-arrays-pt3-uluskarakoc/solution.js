c3daf81d279a3371f7ef3644be2860862b3e824b
ulus karakoc <uluskk@gmail.com>
2023-03-14 21:32:05 +0100

    array

--- 51-pb-arrays-pt3-uluskarakoc/solution.js
+++ 51-pb-arrays-pt3-uluskarakoc/solution.js
@@ -31,9 +31,32 @@ const months = [
 ];
 
 const getMonthName = (num) => {
-  console.log(months[num -1]);
+  if (num <= 12 && num >= 1) {
+    console.log(months[num - 1]);
+  } else {
+    console.log(`Artislik Yapma`);
+  }
 };
 getMonthName(3);
 getMonthName(12);
 getMonthName(6);
+getMonthName(32);
+
+// **3. Amplify the Multiples of 4.**
+result();
+let arr=[]
+const amplifyMultiplesOfFour = (num) => {
+for(let i=0;i<=num;i++){
+ if(i%4===0){
+  let mal10=i*10
+  arr.push(mal10)
+ }else{
+  arr.push(i)
+ }
+
+}
+console.log(arr);
+};
+
+ amplifyMultiplesOfFour(25);
 
