ae47140675bd697b34356236e33fe2337b18ceb8
ulus karakoc <uluskk@gmail.com>
2023-03-15 18:57:10 +0100

    classe Cylinder

--- 52-pb-datastructure-classes-uluskarakoc/index.js
+++ 52-pb-datastructure-classes-uluskarakoc/index.js
@@ -2,15 +2,28 @@ let i = 1;
 const result = () => {
   console.log(`Result----------${i++}`);
 };
-result()
-class Person{
-    constructor(str,age){
-        this.name=str;
-        this.age=age;
-    }
-    describe(){
-     return `${this.name} ${this.age} years old`   
-    }
+result();
+class Person {
+  constructor(str, age) {
+    this.name = str;
+    this.age = age;
+  }
+  describe() {
+    return `${this.name} ${this.age} years old`;
+  }
 }
-const ergebnis= new Person("John",19)
+const ergebnis = new Person("John", 19);
 console.log(ergebnis.describe());
+result();
+// #### 2. Volume
+class Cylinder {
+  constructor(height, radius) {
+    this.height=height;
+    this.radius=radius;
+  }
+  getVolume() {
+    return  this.height*Math.PI*Math.pow(this.radius,2)
+  }
+}
+const volume= new Cylinder(6,4)
+console.log(volume.getVolume());
