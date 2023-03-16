70868a1490f482a82e674733cda3ea4fb60dd986
ulus karakoc <uluskk@gmail.com>
2023-03-16 21:54:27 +0100

    class lernen1

--- 54-pb-classes-exc-2-uluskarakoc/solution.js
+++ 54-pb-classes-exc-2-uluskarakoc/solution.js
@@ -1,8 +1,9 @@
+// your code goes here
+// good luck!
 let i = 1;
 const result = () => {
   console.log(`Result----------${i++}`);
 };
-result();
 // #### 1. Mitarbeiter-Klasse
 class Employee {
   constructor(id, firstName, lastName, taxId, salary) {
@@ -13,13 +14,40 @@ class Employee {
     this.salary = salary;
   }
   generatePaySlip() {
+    let monthlySalary = this.salary / 12;
     return `Employee ID: ${this.id}
 Name: ${this.firstName} ${this.lastName}
 Tax ID: ${this.taxId}
-Pay: ${this.salary}`;
+Pay: ${monthlySalary}`;
   }
 }
-const unternehmen= new Employee(42,"Ulus","Karakoc",56,2000000)
-console.log(unternehmen.generatePaySlip());
-// your code goes here
-// good luck!
+
+// #### 2. Manager Klasse
+
+class Manager extends Employee {
+  constructor(id, firstName, lastName, taxId, salary) {
+    super(id, firstName, lastName, taxId, salary);
+    this.managedEmployees = [];
+  }
+
+  addManagedEmployee(employee) {
+    this.managedEmployees.push(employee);
+  }
+  removeManagedEmployee(employee) {
+    this.managedEmployees = this.managedEmployees.filter(
+      (currentEmployee) => {
+        return currentEmployee.id !== employee.id;
+      } // end filter
+    );
+    // this.managedEmployees.filter(function (e) {
+    //   return e.id !== employee.id;
+    // });
+  }
+}
+
+const anEmployee = new Employee(42, "Ulus", "Karakoc", 56, 2000000);
+const aManager = new Manager(113, "Frank", "Pointyhair", 88454, 90000.0);
+result();
+console.log(anEmployee.generatePaySlip());
+result();
+// console.log(aManager.generatePayslip());
