d562d008a6b1096f90680fb0456116c8b79f392f
ulus karakoc <uluskk@gmail.com>
2023-03-20 13:43:17 +0100

    aufgaben3

--- 56-pb-adv-test-uluskarakoc/solution.js
+++ 56-pb-adv-test-uluskarakoc/solution.js
@@ -4,18 +4,24 @@ const result = () => {
 };
 result();
 // ## Task 1 - Key Value Pairs
-const profileData={
-    firstName:"Kerim",
-    lastName:"Has",
-    age:40,
-    city:"Moskova",
-    details: function (){
-        return `${this.firstName} ${this.lastName} is ${this.age}years old and lives in ${this.city}`
-    }
-
-}
+const profileData = {
+  firstName: "Kerim",
+  lastName: "Has",
+  age: 40,
+  city: "Moskova",
+  details: function () {
+    return `${this.firstName} ${this.lastName} is ${this.age}years old and lives in ${this.city}`;
+  },
+};
 // ## Aufgabe 2 - Objektmethode
 result();
 console.log(profileData.details());
-
-// console.log(profileData);
+// ## Aufgabe 3 - Wortkonverter
+result();
+let adjectives = ["smart", "kind", "sweet", "small", "clear"];
+const wordConverter = (words, suffix) => {
+  for (let i = 0; i < words.length; i++) {
+   console.log(words[i] + suffix);   
+  }
+};
+wordConverter(adjectives, "er");
