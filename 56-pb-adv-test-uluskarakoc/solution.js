9ee380d0c1a8485bf24cb954165912bd0329961f
ulus karakoc <uluskk@gmail.com>
2023-03-20 13:53:44 +0100

    aufgaben5

--- 56-pb-adv-test-uluskarakoc/solution.js
+++ 56-pb-adv-test-uluskarakoc/solution.js
@@ -21,7 +21,24 @@ result();
 let adjectives = ["smart", "kind", "sweet", "small", "clear"];
 const wordConverter = (words, suffix) => {
   for (let i = 0; i < words.length; i++) {
-   console.log(words[i] + suffix);   
+    console.log(words[i] + suffix);
   }
 };
 wordConverter(adjectives, "er");
+// ## Task 4 - Hour Calculation
+result();
+const hourTracking = [
+  { day: "Monday", start: 8, end: 17 },
+  { day: "Tuesday", start: 9, end: 15 },
+  { day: "Wednesday", start: 10, end: 18 },
+  { day: "Thursday", start: 7, end: 14 },
+  { day: "Friday", start: 6, end: 12 },
+];
+const calculateHours = (hours) => {
+    let sum=0;
+   hours.forEach((n) => {
+    sum+=(n.end-n.start);
+  });
+  return sum;
+};
+console.log(calculateHours(hourTracking));
