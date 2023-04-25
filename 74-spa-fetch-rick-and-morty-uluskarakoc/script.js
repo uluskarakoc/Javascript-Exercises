34b4598bb019dab19ea400b68476a75719199e3e
ulus karakoc <uluskk@gmail.com>
2023-04-25 10:15:22 +0200

    async1

--- 74-spa-fetch-rick-and-morty-uluskarakoc/script.js
+++ 74-spa-fetch-rick-and-morty-uluskarakoc/script.js
@@ -1,10 +1,23 @@
 const characterURL = "https://rickandmortyapi.com/api/character/";
 
+// number of characters is 1 to 826
 const rickAndMortyCharacters = async () => {
-    const data = await fetch(characterURL);
-    console.log(data);
-    const json = await data.json();
-    console.log(json)
+    const randomNumber = Math.ceil(Math.random() * 826);
+    const data = await fetch(characterURL + randomNumber);
+    const character = await data.json();
+    console.log(character)
+    console.log(character.image);
+
 }
 
 rickAndMortyCharacters();
+
+
+
+const getCharacterByName = async (name) => {
+    const data = await fetch(`https://rickandmortyapi.com/api/character/?name=${name}`);
+    const character = await data.json();
+    console.log(character);
+}
+
+getCharacterByName("rick");
