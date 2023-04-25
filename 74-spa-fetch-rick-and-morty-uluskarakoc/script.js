c149ca0b071c2e0b6ecc9b600a65f0b66379e539
ulus karakoc <uluskk@gmail.com>
2023-04-25 14:19:28 +0200

    fatiffetc2

--- 74-spa-fetch-rick-and-morty-uluskarakoc/script.js
+++ 74-spa-fetch-rick-and-morty-uluskarakoc/script.js
@@ -7,20 +7,29 @@ const displayErrorInDom = (errorStr) => {
     body.appendChild(errorMsg);
 }
 
+const renderCharacter = (characterObj) => {
+    const imageEl = document.createElement("img");
+    imageEl.src = characterObj.image;
+    body.appendChild(imageEl);
+
+    const nameEl = document.createElement("h1");
+    nameEl.innerText = characterObj.name;
+    body.appendChild(nameEl);
+}
+
 // number of characters is 1 to 826
 const rickAndMortyCharacters = async () => {
     const randomNumber = Math.ceil(Math.random() * 826);
     const data = await fetch(characterURL + randomNumber);
     // Wenn der status Text !== OK ist, dann zeige was anderes im DOM. 
-    if(data.statusText !== "OK"){
+    if(!data.ok){
         const errorStr = `Error: The Database was compromised. No data under the code ${randomNumber}`
         console.log(errorStr);
         displayErrorInDom(errorStr);
     }
     const character = await data.json();
-    console.log(character)
-    console.log(character.image);
-
+    renderCharacter(character);
+    console.log(character);
 }
 
 rickAndMortyCharacters();
