746bbe04d533609ce9d0a7bade775ac50eb027e1
ulus <uluskk@gmail.com>
2024-01-26 21:40:33 +0100

    js wieder4

---
+++ 8-pb-language-number-uluskarakoc/corerefresh.js
@@ -0,0 +1,18 @@
+let i = 1;
+const result = () => {
+  console.log(` Result****${i++}`);
+};
+// Erstelle zwei Variablen introSentence und age. Die erste Variable sollte einen String-Wert enthalten und die zweite eine Zahl. Verkette den String und die Zahl und gib das Ergebnis in der Konsole aus.
+result();
+const introSentence = "Hallo, mein Name ist Ulus und ich bin";
+const age = 32;
+const summe = introSentence + 32;
+console.log(summe);
+// Erstelle eine Variable mit dem Namen `integer` und weise ihr den String-Wert "1005" zu (einschließlich Anführungszeichen). Wandle ihn in eine Zahl um und gib sie in der Konsole aus.
+result();
+const integer = "1005";
+console.log(Number(integer));
+// * Erstelle eine Variable mit dem Namen `float` und weise ihr den String-Wert "10.05" (einschließlich Anführungszeichen) zu. Wandle ihn in eine Zahl um und gib sie in der Konsole aus.
+result();
+const float = "10.05"
+console.log(Number(float));
