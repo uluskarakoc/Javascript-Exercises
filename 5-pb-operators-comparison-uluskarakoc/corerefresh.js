35673eee8703d6255819a3a447e314159336fd2b
ulus <uluskk@gmail.com>
2024-01-26 10:39:29 +0100

    remainder

--- 5-pb-operators-comparison-uluskarakoc/corerefresh.js
+++ 5-pb-operators-comparison-uluskarakoc/corerefresh.js
@@ -1,6 +1,37 @@
 // 1- Check whether apples and oranges are equal.
-const oranges =30
-const apples =20
-console.log(apples===oranges);
+const oranges = 30;
+const apples = 20;
+console.log(apples === oranges);
 // 2- Check whether apples and oranges are not equal.
-console.log(apples!==oranges);
+console.log(apples !== oranges);
+// 3. Check whether `apples` is greater than `oranges`.
+console.log("q3----");
+console.log(apples > oranges);
+// 4. Check whether `apples` is less than or equal to `oranges`.
+console.log("q4----");
+console.log(apples <= oranges);
+// 5. Check whether `oranges` is greater than `apples`.
+console.log("q5----");
+console.log(oranges > apples);
+// 6. Declare another variable `mangoes` and give it a value of 5. Multiply `mangoes` by `apples`, and check whether this result is greater than `mangoes` added to `oranges`.
+console.log("q6----");
+const mangoes = 5;
+const maAp = mangoes * apples;
+console.log(maAp > mangoes + oranges);
+// 7. Subtract `mangoes` from `apples` and check whether this result is less than `oranges` divided by `mangoes`.
+console.log("q7----");
+console.log(apples - mangoes < oranges / mangoes);
+// 8. Check whether `mangoes`, `apples` and `oranges` are equal.
+console.log("q8----");
+console.log((mangoes === apples) === oranges);
+// 9. Check whether the remainder of `apples` divided by `mangoes` and the remainder of `oranges` divided by `mangoes` are equal.
+console.log("q9----");
+const re1 = apples % mangoes;
+const re2 = oranges % mangoes;
+console.log(re1 === re2);
+// 10. Check whether `mangoes` added to `apples` is greater than `oranges` minus `mangoes`. If it is not, find an operator which will give a result of true.
+console.log("q10----");
+console.log(mangoes + apples > oranges - mangoes);
+console.log(mangoes + apples == oranges - mangoes);
+
+// 11. BONUS CHALLENGE: Try increasing script readability by adding question headings to your output before each result.
--- 6-pb-strings-numbers-booleans-uluskarakoc/corerefresh.js
+++ 6-pb-strings-numbers-booleans-uluskarakoc/corerefresh.js
@@ -1,8 +1,32 @@
-const str ="buchholz"
-console.log(str);
-console.log(str[2]);
-console.log(str[5]);
-const firstName = "ulus"
-const age = "33"
-const year = "2024"
-console.log(`${firstName} is ${age} in ${year}`);
+// 1. Deklariere eine Variable namens `str` und weise ihr einen String als Wert zu. Achte darauf, dass der String mindestens 6 Zeichen lang ist. Gib die Variable auf der Konsole aus.
+
+
+// 2. Greife auf das 2. und 6. Element des Strings mit der Indexnotation zu. Gib die Elemente auf der Konsole aus.
+
+
+// 3. Deklariere eine Variable namens `firstName` und weise ihr einen String als Wert zu.
+
+
+// 4. Deklariere eine Variable namens `age` und weise ihr eine Zahl als Wert zu.
+
+
+// 5. Deklariere eine Variable namens `year` und weise ihr eine Zahl als Wert zu.
+
+
+// 6. Gib mit Hilfe deiner deklarierten Variablen einen String auf der Konsole aus. Zum Beispiel: "Basem is 37 in 2020".
+
+
+// 7. Deklariere eine Variable namens `isMarried` und weise ihr einen booleschen Wert zu.
+
+
+// 8. Gib einen String auf der Konsole aus, der angibt, ob deine Person verheiratet ist oder nicht. Zum Beispiel: "Basem is married: true"
+
+
+// 9. Deklariere eine Variable namens `programmingLanguage` und gib ihr den Wert "JavaScript". Deklariere eine Variable namens `isFun` und gib ihr den Wert `true`.  Gib "JavaScript is fun: true" auf der Konsole aus, indem du deine deklarierten Variablen verwendest.
+
+
+// 10. Gib das "J" und "S" von "JavaScript" aus der Variable "Programmiersprache" auf der Konsole aus.
+
+
+// 11. Gib die Länge des Strings "JavaScript" auf der Konsole aus. Noch nicht gemacht? Dann frag eine Suchmaschine deiner Wahl 🙃
+
