19ffe64a4e7856cc98585df6ee13357bdc588480
ulus <uluskk@gmail.com>
2024-01-26 11:45:24 +0100

    js wieder1

--- 6-pb-strings-numbers-booleans-uluskarakoc/corerefresh.js
+++ 6-pb-strings-numbers-booleans-uluskarakoc/corerefresh.js
@@ -7,35 +7,36 @@ const result = () => {
 result();
 const str = "buchholz";
 console.log(str);
-
 // 2. Greife auf das 2. und 6. Element des Strings mit der Indexnotation zu. Gib die Elemente auf der Konsole aus.
 result();
 console.log(str[2]);
 console.log(str[6]);
 // 3. Deklariere eine Variable namens `firstName` und weise ihr einen String als Wert zu.
 result();
-
 const firstName = "ulus";
 // 4. Deklariere eine Variable namens `age` und weise ihr eine Zahl als Wert zu.
 result();
-
 const age = 33;
 // 5. Deklariere eine Variable namens `year` und weise ihr eine Zahl als Wert zu.
 result();
-
 const year = 2024;
 // 6. Gib mit Hilfe deiner deklarierten Variablen einen String auf der Konsole aus. Zum Beispiel: "Basem is 37 in 2020".
 result();
-
 console.log(`${firstName} is ${age} in ${year}`);
 // 7. Deklariere eine Variable namens `isMarried` und weise ihr einen booleschen Wert zu.
 result();
-const isMarried = false
+const isMarried = false;
 // 8. Gib einen String auf der Konsole aus, der angibt, ob deine Person verheiratet ist oder nicht. Zum Beispiel: "Basem is married: true"
 result();
-console.log(`${firstName} is married`);
+console.log(isMarried === true ? "Ulus is married" : "ulus is single");
 // 9. Deklariere eine Variable namens `programmingLanguage` und gib ihr den Wert "JavaScript". Deklariere eine Variable namens `isFun` und gib ihr den Wert `true`.  Gib "JavaScript is fun: true" auf der Konsole aus, indem du deine deklarierten Variablen verwendest.
-
+result();
+const programmingLanguage = "JavaScript";
+const isFun = true;
+console.log(`${programmingLanguage} is fun ${isFun}`);
 // 10. Gib das "J" und "S" von "JavaScript" aus der Variable "Programmiersprache" auf der Konsole aus.
-
+result();
+console.log(programmingLanguage[0] + programmingLanguage[4]);
 // 11. Gib die Länge des Strings "JavaScript" auf der Konsole aus. Noch nicht gemacht? Dann frag eine Suchmaschine deiner Wahl 🙃
+result();
+console.log(programmingLanguage.length);
